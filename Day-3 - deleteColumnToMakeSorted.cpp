d1ba0de032f6d451f5006e8bbbe8d8671f0f7dfb
Muhammad Labeeb Tariq <100291069+phoenixlibbi@users.noreply.github.com>
2023-01-03 05:03:45 -0800

    Add files via upload

---
+++ Day-3 - deleteColumnToMakeSorted.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+ 
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int del=0;
+        for(int i=0;i<strs[0].size();i++){
+            for(int j=0;j<strs.size()-1;j++){
+                if(strs[j][i]>strs[j+1][i]){
+                    del++;
+                    break;
+                }
+            }
+        }
+        return del;
+    }
+};
